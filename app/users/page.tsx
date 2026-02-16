7ef49d2758c0aff36b8f152f8511d685c60362ae
Aderajew Yeshiwendm <adeyeshi294@gmail.com>
2026-02-16 13:28:02 +0300

    fetching in server side

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+interface User {
+  id: number;
+  name: string;
+  
+}
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
 
-const UsersPage = () => {
   return (
     <div>
-      <h1>Hello user</h1>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
       
     </div>
   )
