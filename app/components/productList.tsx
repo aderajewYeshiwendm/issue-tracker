ec0140aa42ee9e31455e591f816eacf34c55b7cc
Aderajew Yeshiwendm <adeyeshi294@gmail.com>
2026-02-16 13:19:20 +0300

    ssr and csr

---
+++ app/components/productList.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AddtoCart from './AddtoCart'
+
+const ProductList = () => {
+  return (
+    <div>
+        <AddtoCart />
+      
+    </div>
+  )
+}
+
+export default ProductList
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import ProductList from "./components/productList";
 export default function Home() {
   return (
     <main>
-
       <h1>Hello World</h1>
       <Link href={"/users"}>users</Link>
+      <ProductList />
 
     </main>
     
